7ca8b7e4066cd499e906bf3eeaccdc82a64fb58b
Adelaja Ibrahim Abiol(AiA) <adelaja444@gmail.com>
2019-05-31 18:26:59 +0100

    Screens created

--- src/Root.js
+++ src/Root.js
@@ -1,51 +1,82 @@
 import React from 'react';
-import {Easing, Animated} from 'react-native'
+import { Easing, Animated } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import LanguageScreen from './screens/LanguageScreen';
+import MainScreen from './screens/MainScreen';
+import LangFormScreen from './screens/LangFormScreen';
+import NumbFormScreen from './screens/NumbFormScreen';
+// import PassFormScreen from './screens/PassFormScreen';
+import NameFormScreen from './screens/NameFormScreen';
+import CompleteFormScreen from './screens/CompleteFormScreen';
 
-// let RootStack = createStackNavigator({
-//    LanguageScreen:{
-//      screen: LanguageScreen,
-//      navigationOptions: ({ navigation }) => ({
-//       header: null
-//     }),
-//    }
-// });
-let RootStack = createStackNavigator({
-   LanguageScreen
-},
-{
-  headerMode: 'none',
-  mode: 'modal',
-  defaultNavigationOptions: {
-    gesturesEnabled: false,
+let RootStack = createStackNavigator(
+  {
+    MainScreen:{
+      screen: MainScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      }),
+    },
+    LangFormScreen:{
+      screen: LangFormScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      }),
+    },
+    NumbFormScreen:{
+      screen: NumbFormScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      }),
+    },
+    // PassFormScreen,
+    NameFormScreen:{
+      screen: NameFormScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      }),
+    },
+    CompleteFormScreen:{
+      screen: CompleteFormScreen,
+      navigationOptions: ({ navigation }) => ({
+        header: null
+      }),
+    }
   },
-  transitionConfig: () => ({
-    transitionSpec: {
-      duration: 300,
-      easing: Easing.out(Easing.poly(4)),
-      timing: Animated.timing,
+  {
+    initialRouteName: 'MainScreen'
+  },
+  {
+    headerMode: 'none',
+    mode: 'modal',
+    defaultNavigationOptions: {
+      gesturesEnabled: false
     },
-    screenInterpolator: sceneProps => {
-      const { layout, position, scene } = sceneProps;
-      const { index } = scene;
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps;
+        const { index } = scene;
 
-      const height = layout.initHeight;
-      const translateY = position.interpolate({
-        inputRange: [index - 1, index, index + 1],
-        outputRange: [height, 0, 0],
-      });
+        const height = layout.initHeight;
+        const translateY = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [height, 0, 0]
+        });
 
-      const opacity = position.interpolate({
-        inputRange: [index - 1, index - 0.99, index],
-        outputRange: [0, 1, 1],
-      });
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1]
+        });
 
-      return { opacity, transform: [{ translateY }] };
-    },
-  }),
-}
+        return { opacity, transform: [{ translateY }] };
+      }
+    })
+  }
 );
 
-export default createAppContainer(RootStack)
+export default createAppContainer(RootStack);
---
+++ src/screens/NameFormScreen/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+// import { View, Text } from 'react-native'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
+
+export class NameFormScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  static propTypes = {
+    prop: PropTypes
+  };
+
+  render() {
+    return (
+        <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>This is Content Section</Text>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NameFormScreen);
